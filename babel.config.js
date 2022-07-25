bff89acd45ebd567fc9383984acade1aa17dde0a
Tom Lamb <tmlamb@gmail.com>
2022-07-25 11:04:33 -0500

    fixing dep mismatches for expo and added console log transform

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,12 @@
 module.exports = api => {
+  const babelEnv = api.env()
   api.cache(true)
+  const plugins = []
+  if (babelEnv === 'production') {
+    plugins.push(['transform-remove-console'])
+  }
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin']
+    plugins: ['react-native-reanimated/plugin', ...plugins]
   }
 }
