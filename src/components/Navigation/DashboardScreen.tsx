655f8eca12a29270cf43f103c7b498ad88d44852
Tom Lamb <tmlamb@gmail.com>
2022-07-29 11:20:02 -0500

    fix re-rendering issue with device theme changes

--- src/components/Navigation/DashboardScreen.tsx
+++ src/components/Navigation/DashboardScreen.tsx
@@ -1,12 +1,24 @@
+import { useFocusEffect } from '@react-navigation/native'
 import React from 'react'
 import useWorkoutStore from '../../hooks/use-workout-store'
 import Dashboard from '../Dashboard'
 import ScreenLayout from './ScreenLayout'
 
 export default function DashboardScreen() {
+  // This forces the Dashboard to always re-render when visited.
+  // This is a kludge to allow device theme changes to reflect
+  // without restarting the app, since otherwise the Dashboard
+  // may never re-render.
+  const [forceRenderKey, setForceRenderKey] = React.useState(0)
+  useFocusEffect(
+    React.useCallback(() => {
+      setForceRenderKey(v => v + 1)
+    }, [])
+  )
+
   const programs = useWorkoutStore(state => state.programs)
   return (
-    <ScreenLayout>
+    <ScreenLayout key={forceRenderKey}>
       <Dashboard programs={programs} />
     </ScreenLayout>
   )
