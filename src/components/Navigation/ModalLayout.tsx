ee5233ea7236f60944b080d4cebff51fdf2e2c35
Tom Lamb <tmlamb@gmail.com>
2022-08-15 15:52:13 -0500

    one more android top padding fix. ugh

--- src/components/Navigation/ModalLayout.tsx
+++ src/components/Navigation/ModalLayout.tsx
@@ -1,6 +1,6 @@
 import { useNavigation } from '@react-navigation/native'
 import React from 'react'
-import { Platform, StatusBar, View } from 'react-native'
+import { Platform, StatusBar } from 'react-native'
 import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context'
 import tw from '../../tailwind'
 import { primaryTextColor } from '../Themed'
@@ -23,12 +23,14 @@ export default function ModalLayout({ children }: { children: React.ReactNode })
   return (
     <SafeAreaView
       style={tw.style(
-        'flex-row justify-center flex-1 bg-white dark:bg-slate-900',
-        `pt-[${Platform.OS === 'android' ? Number(StatusBar.currentHeight) + insets.top : 0}px]`
+        'flex-1 bg-white dark:bg-slate-900',
+        `pt-[${
+          Platform.OS === 'android' ? Number(StatusBar.currentHeight) + insets.top + 24 : 0
+        }px]`
       )}
-      edges={Platform.OS === 'android' ? ['top', 'left', 'right'] : ['left', 'right']}
+      edges={Platform.OS === 'android' ? ['left', 'right'] : ['left', 'right']}
     >
-      <View style={tw`flex-1 max-w-2xl`}>{children}</View>
+      {children}
     </SafeAreaView>
   )
 }
