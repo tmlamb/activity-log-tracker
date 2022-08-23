7ad6466ffefc57ff7ff06d9089fb24eef27e164b
Tom Lamb <tmlamb@gmail.com>
2022-08-22 21:23:14 -0500

    Update App.test.tsx

--- App.test.tsx
+++ App.test.tsx
@@ -6,7 +6,7 @@ describe('<App />', () => {
   it('App root renders with children', async () => {
     const tree = renderer.create(<App />).toJSON() as ReactTestRendererJSON
     await act(async () => {
-      expect(tree?.children?.length).toBe(1)
+      expect(tree?.children?.length).toBe(2)
     })
   })
 })
