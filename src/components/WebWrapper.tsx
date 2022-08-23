50a39dd1f907c91224513b4103fa0989304e983f
Tom Lamb <tmlamb@gmail.com>
2022-08-23 12:22:24 -0500

    refactored web wrapper component

---
+++ src/components/WebWrapper.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View } from 'react-native'
+import tw from '../tailwind'
+
+export default function WebWrapper({ children }: { children: JSX.Element }) {
+  return (
+    <View style={tw`flex-1 web:flex-row web:justify-center web:bg-slate-500`}>
+      <View style={tw`flex-1 web:max-w-2xl bg-slate-50 dark:bg-black`}>{children}</View>
+    </View>
+  )
+}
