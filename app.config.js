61c65472d2c01b757f9970f8e0214dca2502f338
Tom Lamb <tmlamb@gmail.com>
2022-09-01 00:19:54 -0500

    removing broken postPublish hook

--- app.config.js
+++ app.config.js
@@ -1,4 +1,4 @@
-const { APP_ENV, SENTRY_PUBLIC_DSN, SENTRY_ORG, SENTRY_PROJECT, SENTRY_AUTH_TOKEN } = process.env
+const { APP_ENV, SENTRY_PUBLIC_DSN } = process.env
 
 export default ({ config }) => {
   const appConfig = {
@@ -18,19 +18,6 @@ export default ({ config }) => {
       ...config.extra,
       sentryPublicDsn: SENTRY_PUBLIC_DSN,
       appEnv: APP_ENV
-    },
-    hooks: {
-      ...config.hooks,
-      postPublish: [
-        {
-          file: 'sentry-expo/upload-sourcemaps',
-          config: {
-            organization: SENTRY_ORG,
-            project: SENTRY_PROJECT,
-            authToken: SENTRY_AUTH_TOKEN
-          }
-        }
-      ]
     }
   }
   return appConfig
