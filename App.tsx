f01be2c11ecf0418ec7fc947e5baba616e32093e
Tom Lamb <tmlamb@gmail.com>
2022-09-04 11:51:29 -0500

    improved webwrapper children composition

--- App.tsx
+++ App.tsx
@@ -42,10 +42,8 @@ export default function App() {
 
   return (
     <WebWrapper>
-      <>
-        <StatusBar />
-        <Navigation />
-      </>
+      <StatusBar />
+      <Navigation />
     </WebWrapper>
   )
 }
--- src/components/WebWrapper.tsx
+++ src/components/WebWrapper.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { View } from 'react-native'
 import tw from '../tailwind'
 
-export default function WebWrapper({ children }: { children: JSX.Element }) {
+export default function WebWrapper({ children }: { children: React.ReactNode }) {
   return (
     <View style={tw`flex-1 web:flex-row web:justify-center web:bg-slate-500`}>
       <View style={tw`flex-1 web:max-w-2xl bg-slate-50 dark:bg-black`}>{children}</View>
