2e05da542310b4b9be61f188d97f9fddb8c7f9d3
Tom Lamb <tmlamb@gmail.com>
2026-07-25 20:48:00 -0500

    icon improvement

--- apps/expo/src/app/_layout.tsx
+++ apps/expo/src/app/_layout.tsx
@@ -4,12 +4,16 @@ import { useEffect } from "react";
 import { useNativeVariable } from "react-native-css";
 import { KeyboardProvider } from "react-native-keyboard-controller";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import * as SystemUI from "expo-system-ui";
 import { PostHogProvider } from "posthog-react-native";
 
 import useWorkoutStore from "~/hooks/use-workout-store";
 import { captureException, posthog } from "~/utils/posthog";
+
+SplashScreen.preventAutoHideAsync().catch(captureException);
+SplashScreen.setOptions({ fade: true, duration: 400 });
 
 export default function RootLayout() {
   const hasHydrated = useWorkoutStore((state) => state.hasHydrated);
@@ -18,6 +22,12 @@ export default function RootLayout() {
   useEffect(() => {
     SystemUI.setBackgroundColorAsync(backgroundColor).catch(captureException);
   }, [backgroundColor]);
+
+  useEffect(() => {
+    if (hasHydrated) {
+      SplashScreen.hideAsync().catch(captureException);
+    }
+  }, [hasHydrated]);
 
   // Wait for Zustand store hydration before rendering
   if (!hasHydrated) {
